e5c067c1866afbc3a140939f222e0a50955417a1
ElioPaulino <elio_paulino@hotmail.com>
2021-09-26 22:21:43 -0300

    feature/Elio: Reformulaçao e configuração da classe main e primeira tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-//Commit de teste
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Hora do Remédio",
+
+    //Rotas
+    initialRoute: "/primeira",
+    routes: {
+      "/primeira": (context) => PrimeiraTela()
+    },
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class PrimeiraTela extends StatefulWidget {
+  const PrimeiraTela({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _PrimeiraTelaState createState() => _PrimeiraTelaState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _PrimeiraTelaState extends State<PrimeiraTela> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Tela de entrada"),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+        body: Container(
+          padding: EdgeInsets.all(40),
+          
+        )
     );
   }
 }
